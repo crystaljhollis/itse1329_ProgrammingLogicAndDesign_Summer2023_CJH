b552693e990e164dedf1200bfdc19d8858161f90
Crystal Hollis <crystaljhollis@gmail.com>
2023-07-25 06:03:54 -0500

    Crystal Hollis 02.4 Name, Age and Annual Income Assignment for ITSE-1329-53701 2023SU Programming Logic and Design

---
+++ unit02/salary.kt
@@ -0,0 +1,7 @@
+//Name: Crystal Hollis Assignment Name: 02.4 Name, Age and Annual Income Date: 7/25/2023
+fun main() {
+    val name: String = "Crystal Hollis" //my name, Crystal Hollis
+    val age: Int = 32 //my age, 32
+    val annualPay: Double = 180000.0 //my desired salary, $180k
+    println ("My name is $name, my age is $age and I hope to earn $annualPay per year.") //should print as "My name is Crystal Hollis, my age is 32 and I hope to earn $180000.0 per year."
+}
