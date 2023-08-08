d6b1698d8e8419227a0e48125d49b961dbf282dd
Crystal Hollis <crystaljhollis@gmail.com>
2023-08-08 04:40:18 -0500

    04.1 Roman Numerals
    Unit 3 (or 4...?) Assignment

--- unit03/romans.kt
+++ unit03/romans.kt
@@ -1,37 +1,34 @@
+// Name: Crystal Hollis | Assignment Name: 04.1 Roman Numerals | Date: 8/8/2023
 package unit03
 fun main() {
     // 1. The program should prompt the user to input a number within the inclusive range of 1 through 10.
-    println("Enter a number that is within 1 through 10:")
-    // readLine - Reads a line of input from the standard input stream
-    // toIntOrNull - Parses string as Int number or null if the string is not a valid number
-    val userInput = readLine()?.toIntOrNull()
+    println("Enter a valid number between 1 and 10:")
+    val userInput = readLine()     //readLine - Reads a line of input
+    //  Input Validation criteria - The program should validate user input ensuring it is within range of 1 through 10.
+    val number = userInput?.toIntOrNull()     // toIntOrNull - parses string as integer or null if not a number
 
-    if (userInput != null) {
-        if (userInput in 1..10) {
-            // Convert the number to Roman numeral using if-else statements
-            val romanNumeral = when (userInput) {
-                1 -> "I"
-                2 -> "II"
-                3 -> "III"
-                4 -> "IV"
-                5 -> "V"
-                6 -> "VI"
-                7 -> "VII"
-                8 -> "VIII"
-                9 -> "IX"
-                else -> "X"
-            }
-
-            // Display the Roman numeral equivalent
-            println("The Roman numeral equivalent of $userInput is $romanNumeral")
-        } else {
-            // Display an error message for numbers outside the range
-            println("Error: Please enter a number between 1 and 10.")
-        }
+    // 2. After accepting user input, the program should display the Roman numeral equivalent using only `if` and
+    // `else` statements to make the determination. of the entered number. In the event that the entered number is
+    // outside the range of 1 through 10, the program should display an error message.
+    // Error Handling criteria - The program should handle errors gracefully, displaying an appropriate error message.
+    if (number == null || number < 1 || number > 10) {  // '||' logical OR
+        println("Error: Please enter a valid number between 1 and 10.")
     } else {
-        // Display an error message for invalid input
-        println("Error: Invalid input. Please enter a valid number.")
-    }
-}
+        // Roman Numeral Calculation Criteria - The program should correctly determine the Roman numeral equivalent
+        // of the input number using only `if` and `else` statements.
+        val romanNumeral = if (number == 1) "I"
+        else if (number == 2) "II"
+        else if (number == 3) "III"
+        else if (number == 4) "IV"
+        else if (number == 5) "V"
+        else if (number == 6) "VI"
+        else if (number == 7) "VII"
+        else if (number == 8) "VIII"
+        else if (number == 9) "IX"
+        else "X"
 
+        // Output Formatting Criteria - The program should display the Roman numeral equivalent of the input number
+        // in a clear and readable format.
+        println("The Roman numeral equivalent of $number is $romanNumeral.")
+    }
 }
